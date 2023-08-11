dc0b2783a14b8eb7a70e60d4f8d257b1ce77737c
ericgolston <egolston.562@gmail.com>
2023-08-11 14:02:36 -0700

    Implement complete and edit buttons with final touches

--- gui.py
+++ gui.py
@@ -1,19 +1,28 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",  # Creates list from text file after reading
-					  enable_events=True, size=[45, 10])
+					  enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-				   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+				   layout=[[clock],
+						   [label],
+						   [input_box, add_button],
+						   [list_box, edit_button, complete_button],
+						   [exit_button]],
 				   font=('Helvetica', "20"))
 
 while True:
-	event, values_dict = window.read()
+	event, values_dict = window.read(timeout=10)
+	window['clock'].update(value=time.strftime("%b %d %Y %H:%M:%S"))
 	print(1, event)
 	print(2, values_dict)
 	print(3, values_dict['todos'])
@@ -27,21 +36,34 @@ while True:
 			window['todo'].update(value="")
 
 		case "Edit":
-			todo_to_edit = values_dict['todos'][0]  # Different dictionary
-			new_todo = values_dict['todo']
+			try:
+				todo_to_edit = values_dict['todos'][0]  # Different dictionary
+				new_todo = values_dict['todo']
 
+				todos = functions.get_todos()
+				index = todos.index((todo_to_edit))  # Index of line clicked
+				todos[index] = new_todo
+				functions.write_todos(todos)
+				window['todos'].update(values=todos)
+			except IndexError:
+				sg.Popup("Please select an item first.")
 
-			todos = functions.get_todos()
-			index = todos.index((todo_to_edit)) # Index of line clicked
-			todos[index] = new_todo
-			functions.write_todos(todos)
-			window['todos'].update(values=todos)
-
+		case "Complete":
+			try:
+				todo_to_complete = values_dict['todos'][0]
+				todos = functions.get_todos()
+				todos.remove(todo_to_complete)
+				functions.write_todos(todos)
+				window['todos'].update(values=todos)
+				window['todo'].update(value='')
+			except IndexError:
+				sg.Popup("Please select an item first.")
 
 		case 'todos':
 			window['todo'].update(value=values_dict['todos'][0])
 
-
+		case 'Exit':
+			break
 		case sg.WIN_CLOSED:
 			break
 
